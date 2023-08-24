68619cc07e3fced6087ee866b98a6c89138e9cd0
lucasnerytoledo <lucas.leite@petlove.com.br>
2023-08-24 11:13:06 -0300

    init

---
+++ src/test/java/app/lucas/meusgastos/MeusgastosApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lucas.meusgastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusgastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
